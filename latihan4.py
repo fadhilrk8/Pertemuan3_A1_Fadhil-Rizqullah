6b6369ca48dd6d82164974ecda474ff628a7fc0b
fadhilrk8 <143146204+fadhilrk8@users.noreply.github.com>
2023-08-28 15:46:18 +0700

    Add files via upload

---
+++ latihan4.py
@@ -0,0 +1,12 @@
+str = "Python"
+print(str)
+str2 = 'Py"thon'
+print(str2)
+teks = "\"OK, \" sampai ketemu lagi"
+print(teks)
+teks = """Python adalah bahasa pemrograman yang powerfull.\nTerbukti Python bisa dijalankan di segala platform OS. \nJadi, saatnya kita menggunakan Python sebagai bahasa pemrograman sehari-hari. Salam Python Dahsyat!"""
+kata = """Python adalah bahasa pemrograman yang powerfull.
+... Terbukti Python bisa dijalankan di segala platform OS
+... Jadi, saatnya kita menggunakan Python sebagai bahasa pemrogaman
+...Sehari-hari. Salam Python Dahsyat!"""
+print(kata)
